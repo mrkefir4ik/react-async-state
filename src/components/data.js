0db0b13205a149da1d50394fad2b0992d7e8537d
Daniil Aharodnikau <Daniil.Aharodnikau@itechart-group.com>
2021-10-12 12:34:04 +0300

    fix: made search manual

--- src/components/data.js
+++ src/components/data.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { useState } from 'react';
 // import { connect } from 'react-redux'
 
 const  Data = (props) => {
 
-    const [searchValue, setSearchValue] = useState()
     let input = React.createRef();
 
     const onChangeHandler =() =>{
-      console.log(searchValue);
-      setSearchValue(input.current.value);
+      console.log(input.current.value);
+      props.onInputChange(input.current.value);
     }
 
     const onClickHandler =()=>{
-      props.onClickFetch(searchValue)
+      props.onInputChange(input.current.value);
+      props.onClickFetch()
     }
     const handleKeypress = e => {
         //it triggers by pressing the enter key
       if (e.charCode === 13) {
-        props.onClickFetch(input);
+        props.onInputChange(input.current.value)
+        props.onClickFetch()
       }
     };
 
